a4a90779a47d75ab0f4e9737a255201d54d7349d
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-10 13:06:20 -0700

    further study sort triciery

--- wordcount.py
+++ wordcount.py
@@ -1,6 +1,7 @@
 # put your code here.
 import sys
 from collections import Counter
+from operator import attrgetter
 
 my_file = open(sys.argv[1])
 count_words = {}
@@ -27,8 +28,12 @@ count_words = Counter(better_words)
 # for word in sorted(count_words):
 #      print (f'{word} {count_words[word]}')      #sorting alphabetically
 
-for word in sorted(count_words, key=lambda word: count_words[word]):
-    print (f'{word} {count_words[word]}')         #sorting by value (integers -ascending order)
+# for word in sorted(count_words, key=lambda word: count_words[word]):
+#     print (f'{word} {count_words[word]}')         #sorting by value (integers -ascending order)
 
+new_count_words = sorted(count_words, key=lambda word: count_words[word], reverse=True)
+
+for word in sorted(new_count_words, key = attrgetter('word')):
+    print (f'{word} {count_words[word]}')         #sorting by value (integers -descending order)
 
 my_file.close()
